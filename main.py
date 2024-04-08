5efe56045c0c7ed26a6ea46e9f2e294f2ddc4258
Viet Duc Do <duc.do32@mailbox.org>
2024-04-08 13:55:26 +1200

    Endpoint mehtods put, delete

--- main.py
+++ main.py
@@ -21,6 +21,20 @@ class User(BaseModel):
 def get_users():
     return users
 
+@app.post("/users")
+def create_user(user: User):
+    # declare variables
+    # formatting user to a dict
+    # append dict to list users
+    new_user = {
+        "id": str(uuid.uuid4()),
+        "username": user.username,
+        "email": user.email
+    }
+    users.append(new_user)
+    return new_user, 201
+    
+
 @app.get("/users/{user_id}")
 def get_user(user_id: str):
     for user in users:
@@ -28,6 +42,23 @@ def get_user(user_id: str):
             return user
     return {"message": "User not found"}
 
+@app.put("/users/{user_id}")
+def update_user(user_id: str, user: User):
+    for u in users:
+        if u["id"] == user_id:
+            u["username"] = user.username
+            u["email"] = user.email
+            return u, 201
+    return {"message": "User not found"}
+
+@app.delete("/users/{user_id}")
+def delete_user(user_id):
+    for i, u in enumerate(users):
+        if u["id"] == user_id:
+            users.pop(i)
+            return 201
+    return {"message": "User does not exist"}
+            
 
 if __name__ == "__main__":
     uvicorn.run(app, host="127.0.0.1", port=8000)    
